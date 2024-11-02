e159b601082152b74da5c03d56cf9bafde74c3c4
gkielian <gkielian@users.noreply.github.com>
2024-11-01 20:21:50 -0700

    Add back plot_statistics
    Will discuss refactoring later (need this in separate util folder).

--- train.py
+++ train.py
@@ -1249,6 +1249,9 @@ class Trainer:
                         if self.args.max_sample_tokens:
                             self.sample_and_print(self.args.max_sample_tokens, start_tokens=self.args.sample_start_tokens)
                     break
+                    
+            if self.args.plot_statistics:
+                plot_statistics(self.args, self.stats, graph_y_labels)
 
             if self.args.tensorboard_log:
                 self.writer.flush()
