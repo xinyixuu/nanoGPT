fa0a94826aab59183d48a002de5ec1114d0b0e2b
Xinyi Xu <xinyixuu@umich.edu>
2024-11-04 19:22:46 +0000

    Deleted intermediate json file, instead downloading them from HF

--- data/snac/tts_train.sh
+++ data/snac/tts_train.sh
@@ -8,6 +8,22 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 echo "Running text to speech training..."
 
+url="https://huggingface.co/datasets/xinyixuu/tiny_sherlock_whisper_snac_combined"
+out_dir="tiny_sherlock_whisper_snac_combined"
+
+if [[ ! -d "${out_dir}" ]]; then
+  mkdir -p "${out_dir}"
+fi
+
+for i in $(seq -f "%02g" 1 24); do
+  for j in $(seq -f "%02g" 0 3); do
+    wget -nc "${url}/resolve/main/tiny_sherlock_whisper_snac_combined/tiny_sherlock_audio_${i}_part0${j}.json?download=true" || {
+      echo "File tiny_sherlock_audio_${i}_part0${j}.json not found, skipping."
+      continue
+    }
+  done
+done
+
 # Get the input file for running the dataset
 python3 extract_data.py "tiny_sherlock_whisper_snac_combined" "input.txt" --directory
 
