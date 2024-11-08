38be608a9c58bff5427c187e9eb826a9b36c0871
Michael Moffatt <mmoffatt@umich.edu>
2024-11-08 11:34:40 -0500

    fixed test script

--- tests/test_quantization_cpu.sh
+++ tests/test_quantization_cpu.sh
@@ -20,53 +20,50 @@ timestamp="$(date +%F_%T)"
 notes="check_all_quantization_options"
 run_name="${dataset}_quantization_${max_iters}_${block_size}_${n_layer}_${n_head}_${n_embd}_${notes}"
 
-# Loop over the array
-do
-  output_dir="results/${timestamp}_${notes}_quantization"
-  if [ ! -d "${output_dir}" ]; then
-    mkdir -p "${output_dir}"
-  fi
+output_dir="results/${timestamp}_${notes}_quantization"
+if [ ! -d "${output_dir}" ]; then
+  mkdir -p "${output_dir}"
+fi
 
-  python3 train.py \
-    --max_iters "$max_iters" \
-    --n_layer "$n_layer" \
-    --n_head "$n_head" \
-    --n_kv_group "$n_kv_group" \
-    --n_embd "$n_embd" \
-    --eval_iters "$eval_iters" \
-    --eval_interval "$eval_interval" \
-    --log_interval 10 \
-    --device cpu \
-    --dataset "$dataset" \
-    --quantize_linear_method "$quant_method" \
-    --activations_quant_method "$quant_method" \
-    --quantization_warmup_iters 0 \
-    --quantize_attn_act \
-    --quantize_mlp_act \
-    --linear_variant_attn "$linear_variant" \
-    --linear_variant_mlp "$linear_variant" \
-    --store_activations \
-    --tensorboard_run_name "$run_name" \
-    --block_size "$block_size" \
-    --out_dir "${output_dir}"
+python3 train.py \
+  --max_iters "$max_iters" \
+  --n_layer "$n_layer" \
+  --n_head "$n_head" \
+  --n_kv_group "$n_kv_group" \
+  --n_embd "$n_embd" \
+  --eval_iters "$eval_iters" \
+  --eval_interval "$eval_interval" \
+  --log_interval 10 \
+  --device cpu \
+  --dataset "$dataset" \
+  --quantize_linear_method "$quant_method" \
+  --activations_quant_method "$quant_method" \
+  --quantization_warmup_iters 0 \
+  --quantize_attn_act \
+  --quantize_mlp_act \
+  --linear_variant_attn "$linear_variant" \
+  --linear_variant_mlp "$linear_variant" \
+  --store_activations \
+  --tensorboard_run_name "$run_name" \
+  --block_size "$block_size" \
+  --out_dir "${output_dir}"
 
-  python3 sample.py \
-    --out_dir "${output_dir}" \
-    --device "cpu" \
-    --num_samples 1 \
-    --max_new_tokens 100 \
-    --start "What great fortune this is"
+python3 sample.py \
+  --out_dir "${output_dir}" \
+  --device "cpu" \
+  --num_samples 1 \
+  --max_new_tokens 100 \
+  --start "What great fortune this is"
 
-  python3 save_weights.py \
-    --out_dir "${output_dir}" \
-    --file_name "quantized_data" \
-    --file_type "pkl"
+python3 save_weights.py \
+  --out_dir "${output_dir}" \
+  --file_name "quantized_data" \
+  --file_type "pkl"
 
-  python3 quantization/visualize.py \
-  --file_name "quantized_data.pkl" \
-  --image_folder "quantized_images" \
-  --weight "all" \
-  --graph="histogram"
+python3 quantization/visualize.py \
+--file_name "quantized_data.pkl" \
+--image_folder "quantized_images" \
+--weight "all" \
+--graph="histogram"
 
-  sleep 3
-done
+sleep 3
