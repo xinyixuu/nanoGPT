eb684603318d676456d4b0fdfb66419977eb6f8f
Xinyi Xu <xinyixuu@umich.edu>
2025-03-10 03:09:21 +0000

    add scripts to run IPA multidataset experiments

---
+++ demos/multidataset_demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get current script directory
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+# get into main directory
+pushd "$script_dir/../" > /dev/null
+
+# tokenize on commonvocie_zh
+bash data/commonvoice_zh/get_dataset.sh
+
+# tokenize on commonvocie_ko
+bash data/commonvoice_ko/get_dataset.sh
+
+# tokenize on commonvoice_ja
+bash data/commonvoice_ja/get_dataset.sh
+
+# running the training
+python3 optimization_and_search/run_experiments.py --config explorations/multidataset.json --output_dir out_muti_zh
+popd
