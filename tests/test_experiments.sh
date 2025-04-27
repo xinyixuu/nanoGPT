4beaf0b4f6dcc438c04b8859c517e474cf4dedce
klei22 <kauna@avalonwest.tech>
2025-04-26 20:49:57 -0700

    Update CI to include tests with runexp yaml

--- tests/test_experiments.sh
+++ tests/test_experiments.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # test_experiments.sh
 
+dataset="shakespeare_char"
+python3 "data/${dataset}/prepare.py"
+
 pushd ../
 python3 optimization_and_search/run_experiments.py -c tests/run_optimization_tests/test_range.yaml         --config_format yaml
 python3 optimization_and_search/run_experiments.py -c tests/run_optimization_tests/test_lists.yaml         --config_format yaml
