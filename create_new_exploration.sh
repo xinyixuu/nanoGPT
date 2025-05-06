44030e4e4d5d49de4c3a6024ad58b35e323c8347
klei22 <kauna@avalonwest.tech>
2025-05-05 20:14:18 -0700

    Add automatic renaming of sample.yaml in title
    Will rename the title of the newly created exploration in the in-file
    title.

--- create_new_exploration.sh
+++ create_new_exploration.sh
@@ -4,3 +4,4 @@
 new_exploration_name="$1"
 
 cp ./explorations/sample.yaml "./explorations/${new_exploration_name}.yaml"
+sed -i "s/sample.yaml/${1}.yaml/g" "./explorations/${new_exploration_name}.yaml"
