d3473d424d6f1110bd2c176f4676d6ddf76fa0c5
Gregory Kielian <gkielian@google.com>
2025-05-13 06:14:23 +0000

    Setup hypersphere packer for shakespeare char

--- analysis/distillation/run_hypersphere_packer.sh
+++ analysis/distillation/run_hypersphere_packer.sh
@@ -1,5 +1,5 @@
-python hypersphere_shadow_packing.py --dim 64 --k 50000 \
+python hypersphere_shadow_packing.py --dim 384 --k 65 \
     --optimizer SGD --step-size 1.0 \
     --max-iter 100000 \
-    --plot-every 5000 \
-    --logdir logs_adam_cosine_2
+    --plot-every 10000 \
+    --logdir logs_embd_table
