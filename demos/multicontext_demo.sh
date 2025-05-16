585d4da77b710c049714381394aaf9de77a6f378
klei22 <kauna@avalonwest.tech>
2025-05-15 23:15:19 -0700

    Modify sample.py to support multicontext sampling
    Also added an example of sample.py invocation to the demos folder.
    
    Next steps are to create a function for this that can be called from
    train.py, however this much should unblock future experiments, for now.

--- demos/multicontext_demo.sh
+++ demos/multicontext_demo.sh
@@ -11,6 +11,30 @@ python3 train.py \
       ./data/shakespeare_char_punct \
       ./data/shakespeare_char_order \
    --vocab_sizes 65 256 20 4 24 \
-   --max_iters 6000 \
+   --max_iters 3000 \
+   --dropout 0.2 \
    --save_major_ckpt_interval 250 \
-   --use_rotary_embeddings
+   --use_rotary_embeddings \
+   --compile
+
+python3 sample.py \
+--out_dir ./out \
+--multicontext \
+--multicontext_datasets \
+    shakespeare_char \
+    shakespeare_char_mobius \
+    shakespeare_char_pos_2 \
+    shakespeare_char_punct \
+    shakespeare_char_order \
+--multicontext_start \
+    "Second " \
+    "1234567" \
+    "aaaaaa " \
+    "212122_" \
+    "123456_" \
+--max_new_tokens \
+    256 \
+--top_k \
+    1 10 \
+--num_samples 1
+python3 sample.py --out_dir ./out --multicontext --multicontext_datasets  shakespeare_char shakespeare_char_mobius shakespeare_char_pos_2 shakespeare_char_punct shakespeare_char_order  --multicontext_start  "Second " "1234567" "aaaaaa " "212122_" "123456_" --max_new_tokens 512 --top_k 1 --num_samples 1
