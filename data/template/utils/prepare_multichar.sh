e20bbadad20bc700a2ef630c0b3378696027d85f
klei22 <kauna@avalonwest.tech>
2025-05-22 23:45:36 -0700

    Add fixes for the multichar script
    Increases efficiency with --reuse_chars and --skip_tokenization.

--- data/template/utils/prepare_multichar.sh
+++ data/template/utils/prepare_multichar.sh
@@ -12,13 +12,16 @@ for i in ${arr[@]}; do
   cp input.txt "$filename"
   # convert chars
   python3 ./utils/char_convert.py "$filename" --method "$i"
+  # create meta.pkl dictionary, but skip creation of bin
+  python3 prepare.py -t "tokensfile.txt" --method char --skip_tokenization
   # tokenize, creating bins and meta.pkl files
-  python3 prepare.py -t "$filename" --method char
+  python3 prepare.py -t "$filename" --method char --reuse_char
   # move to dir
   dirname="mc_$i"
   mkdir -p "$dirname"
   mv "$filename" "$dirname"
   mv *.bin "$dirname"
   mv meta.pkl "$dirname"
+  mv tokensfile.txt "$dirname"
 done
 
