06621c2c47aa86318b02d9ed67a0dd33cda63158
klei22 <kauna@avalonwest.tech>
2025-06-01 00:35:08 -0700

    Change time_remaining_mode to eta_variant
    This is much easier to remember, and also resembles the name of the
    train_variations/eta_variants.py script.

--- train_args.py
+++ train_args.py
@@ -37,7 +37,7 @@ def parse_args():
     training_group.add_argument('--eval_only', default=False, action=argparse.BooleanOptionalAction)
 
     # latency / ETA estimate options
-    training_group.add_argument('--time_remaining_mode', choices=['iteration', 'eval_cycle'], default='eval_cycle', help="iteration - estimates only based on training iterations -- use if doing one eval at the end; eval_cycle -- use if doing multiple evals, will use a single cycle for the estimation.")
+    training_group.add_argument('--eta_variant', choices=['iteration', 'eval_cycle'], default='eval_cycle', help="iteration - estimates only based on training iterations -- use if doing one eval at the end; eval_cycle -- use if doing multiple evals, will use a single cycle for the estimation.")
     training_group.add_argument('--iteration_window', default=100, type=int)
     training_group.add_argument('--eval_cycle_window', default=5, type=int)
 
